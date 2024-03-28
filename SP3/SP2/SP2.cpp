6f844bde6ef23ee8ee70a56df5f1f033c0bf730f
VikaBalandina <145467361+VikaBalandina@users.noreply.github.com>
2024-03-28 20:16:07 +0500

    Add files via upload

---
+++ SP3/SP2/SP2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    std::cout << "Ex1 \n";
+    int num;
+
+    std::cout << "Enter a number to calculate the factorial : ";
+    std::cin >> num;
+
+    int result = factorial(num);
+    std::cout << "Factorial of a number " << num << " is " << result << endl;
+
+
+    std::cout << "\nEx2 \n";
+
+    float N, M;
+
+    std::cout << "Enter number \"N\": ";
+    std::cin >> N;
+    std::cout << "Enter degree \"M\": ";
+    std::cin >> M;
+    cout << "The number "<< N << " to the degree of "<< M << " is equal to " << pow(N, M)<< "\n";
+
+    return 0;
+
+}
+
