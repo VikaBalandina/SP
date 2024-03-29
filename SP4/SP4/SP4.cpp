9981f7df32c27256caf43bdbeaf13e3278d81bf1
VikaBalandina <145467361+VikaBalandina@users.noreply.github.com>
2024-03-29 18:40:30 +0500

    Add files via upload

---
+++ SP4/SP4/SP4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+
+    std::cout << "Введите количество элементов: ";
+    std::cin >> n;
+
+    int arr[20] ;
+    
+    std::cout << "Введите элементы:" << std::endl;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+        sum += arr[i];
+    }
+
+    std::cout << "Сумма элементов: " << sum << std::endl;
+
+    return 0;
+}
