5cfb008b4a699023538bc57e9d78023b5e911127
VikaBalandina <145467361+VikaBalandina@users.noreply.github.com>
2024-03-29 19:16:55 +0500

    Add files via upload

---
+++ SP5/SP5/SP5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    int rows, colums, sum = 0;
+
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> colums;
+
+    int arr[20][20];
+
+    std::cout << "Введите элементы n: ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < colums; j++) {
+            std::cin >> arr[i][j];
+            sum += arr[i][j];
+        }
+    }
+
+    std::cout << "Сумма элементов: " << sum << std::endl;
+
+    return 0;
+}
